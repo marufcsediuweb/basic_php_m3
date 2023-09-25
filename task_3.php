57080b22010adfe58872500d71d5bc83d8ac80dc
arionmaruf <maruf@marufabdullah.com>
2023-09-25 21:32:20 +0600

    Task 03 submitted.

---
+++ task_3.php
@@ -0,0 +1,18 @@
+<?php
+// Array Sorting
+
+$grades = [85, 92, 78, 88, 95];
+//$grades2 = [80, 46, 33, 82, 99, 56]; // added for testing function
+function printGrades($grades)
+{
+    rsort($grades);
+    print_r($grades);
+}
+
+printGrades($grades);
+//print_r($grades); // Main Array unchanged as the operation is inside the function's local scope
+//printGrades($grades2); // added for testing function
+
+
+
+?>
